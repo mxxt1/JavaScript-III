d43be6519037fe9d855d04c9044589f4e13687ca
mxxt1 <matt@dustcloudlabs.com>
2019-08-22 14:59:26 -0400

    expanded answers to four principles

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. Global
-* 2. Implicit
-* 3. Explicit
-* 4. Constructor
+*   
+* 1. Window --> this refers to the global scope and window binding.
+* 2. Implicit --> this refers to the object on which the method is called (ie, this refers to the object left of the dot)
+* 3. Constructor --> this refers to the object created using the constructor function and 'new' keyword/
+* 4. Explicit --> the object is specifically identified using the call/apply/bind methods. 'this' refers to the object that has been .called(object), .apply(object), or .bind(object).
 *
 * write out a code example of each explanation above
 */
@@ -26,7 +26,7 @@ let exampleObj = {
     name: "example object",
     purpose: "provide an example of implicit binding",
     testify: function(){
-        console.log(`I am an ${this.name} and my purpose is to ${this.purpose}`);
+        console.log(`I am an ${this.name} and my purpose is to ${this.purpose}.`);
     }
 }
 
@@ -38,7 +38,7 @@ exampleObj.testify();
 // code example for New Binding
 
 function Example(passedData){
-    this.canBeAnything = passedData;
+   return this.canBeAnything = passedData;
 }
 
 let newExample = new Example("this is what is passed to the function");
